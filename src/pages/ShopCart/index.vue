f07789fb1471690368cbdb340cafdbb9865b5a70
enheng1238 <223547544@qq.com>
2020-09-17 10:53:37 +0800

    disNum 修正

--- src/pages/ShopCart/index.vue
+++ src/pages/ShopCart/index.vue
@@ -13,27 +13,34 @@
       <div class="cart-body">
         <ul class="cart-list" v-for="(cart,index) in shopCartList" :key="cart.id">
           <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" :checked="cart.isChecked">
+            <input type="checkbox" name="chk_list" :checked="cart.isChecked" />
           </li>
           <li class="cart-list-con2">
-            <img :src="cart.imgUrl">
+            <img :src="cart.imgUrl" />
             <div class="item-msg">{{cart.skuName}}</div>
           </li>
-          
+
           <li class="cart-list-con4">
             <span class="price">{{cart.skuPrice}}</span>
           </li>
           <li class="cart-list-con5">
-            <a href="javascript:void(0)" class="mins">-</a>
-            <input autocomplete="off" type="text" :value="cart.skuNum" minnum="1" class="itxt">
-            <a href="javascript:void(0)" class="plus">+</a>
+            <a href="javascript:void(0)" class="mins" @click="changeCartNum(cart,-1)">-</a>
+            <input
+              autocomplete="off"
+              type="text"
+              :value="cart.skuNum"
+              minnum="1"
+              class="itxt"
+              @change="changeCartNum(cart,$event.target.value*1)"
+            />
+            <a href="javascript:void(0)" class="plus" @click="changeCartNum(cart,1)">+</a>
           </li>
           <li class="cart-list-con6">
             <span class="sum">{{cart.skuPrice * cart.skuNum}}</span>
           </li>
           <li class="cart-list-con7">
             <a href="#none" class="sindelet">删除</a>
-            <br>
+            <br />
             <a href="#none">移到收藏</a>
           </li>
         </ul>
@@ -41,7 +48,7 @@
     </div>
     <div class="cart-tool">
       <div class="select-all">
-        <input class="chooseAll" type="checkbox" v-model="isAllCheck">
+        <input class="chooseAll" type="checkbox" v-model="isAllCheck" />
         <span>全选</span>
       </div>
       <div class="option">
@@ -50,8 +57,10 @@
         <a href="#none">清除下柜商品</a>
       </div>
       <div class="money-box">
-        <div class="chosed">已选择
-          <span>{{checkedNum}}</span>件商品</div>
+        <div class="chosed">
+          已选择
+          <span>{{checkedNum}}</span>件商品
+        </div>
         <div class="sumprice">
           <em>总价（不含运费） ：</em>
           <i class="summoney">{{allMoney}}</i>
@@ -65,279 +74,289 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
-  export default {
-    name: 'ShopCart',
-    mounted() {
-      this.getShopCartList()
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "ShopCart",
+  mounted() {
+    this.getShopCartList();
+  },
+  methods: { 
+    getShopCartList() {
+      this.$store.dispatch("getShopCartList");
     },
-    methods: {
-      getShopCartList(){
-        this.$store.dispatch('getShopCartList')
+
+    // ...mapActions(['getShopCartList'])
+
+   async changeCartNum(cart,disNum){
+      if(disNum + cart.skuNum < 1){
+        disNum = 1 - cart.skuNum  //最少得是1   
       }
+      try{
+        await this.$store.dispatch('addOrUpdateShopCart',{skuId:cart.skuId,skuNum:disNum})
+        this.getShopCartList()
+      }catch(error){
+        alert('修改购物车商品数量失败',eeror.message)
+      }
+     
+    }
+  },
+  computed: {
+    ...mapState({
+      shopCartList: (state) => state.shopcart.shopCartList,
+    }),
 
-      // ...mapActions(['getShopCartList'])
-    },
-    computed: {
-      ...mapState({
-        shopCartList: state => state.shopcart.shopCartList
-      }),
-      
-      checkedNum(){
-        // 统计 reduce  数组的方法 
-        /**
-         * 函数的三要素：功能 参数 返回值
-         * reduce
-         * 功能：统计数组当中符合条件的项  计算总数或者其他
-         * 参数：第一个是一个回调函数，回调函数当中又有四个参数，一般写前三个(上一次统计的值,每一项,每一项的下标,数组本身)
-         * 第二个参数代表统计的初始值,循环数组的第一项上一次统计的值拿的就是统计的初始值
-         *  this.shopCartList.reduce((prev,item,index)=>{
-         *
-         *   },0)
-         * 
-         * 返回值: 统计完成后的结果，也就是最后一次统计的结果
-         */
-        return this.shopCartList.reduce((prev,item)=>{
-          if(item.isChecked){
-            prev += item.skuNum;
-          }
-          return prev
-        },0)
-      },
-      allMoney(){
-        return this.shopCartList.reduce((prev,item)=>{
-          if(item.isChecked){
-            prev += item.skuNum * item.skuPrice
-          }
-          return prev
-        },0)
-      },
-
-      isAllCheck:{
-        /**
-         * 如果只有get没有set 可以简写成函数的形式
-         * 如果get和set都有,只能写成对象的形式
-         */
-        get(){
-           return this.shopCartList.every(item => item.isChecked) && (this.shopCartList.length >0)
-        },
-        set(){
-
+    checkedNum() {
+      // 统计 reduce  数组的方法
+      /**
+       * 函数的三要素：功能 参数 返回值
+       * reduce
+       * 功能：统计数组当中符合条件的项  计算总数或者其他
+       * 参数：第一个是一个回调函数，回调函数当中又有四个参数，一般写前三个(上一次统计的值,每一项,每一项的下标,数组本身)
+       * 第二个参数代表统计的初始值,循环数组的第一项上一次统计的值拿的就是统计的初始值
+       *  this.shopCartList.reduce((prev,item,index)=>{
+       *
+       *   },0)
+       *
+       * 返回值: 统计完成后的结果，也就是最后一次统计的结果
+       */
+      return this.shopCartList.reduce((prev, item) => {
+        if (item.isChecked) {
+          prev += item.skuNum;
         }
-      }
-      
+        return prev;
+      }, 0);
     },
-  }
+    allMoney() {
+      return this.shopCartList.reduce((prev, item) => {
+        if (item.isChecked) {
+          prev += item.skuNum * item.skuPrice;
+        }
+        return prev;
+      }, 0);
+    },
+
+    isAllCheck: {
+      /**
+       * 如果只有get没有set 可以简写成函数的形式
+       * 如果get和set都有,只能写成对象的形式
+       */
+      get() {
+        return (
+          this.shopCartList.every((item) => item.isChecked) &&
+          this.shopCartList.length > 0
+        );
+      },
+      set() {},
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .cart {
-    width: 1200px;
-    margin: 0 auto;
+.cart {
+  width: 1200px;
+  margin: 0 auto;
 
-    h4 {
-      margin: 9px 0;
-      font-size: 14px;
-      line-height: 21px;
-    }
+  h4 {
+    margin: 9px 0;
+    font-size: 14px;
+    line-height: 21px;
+  }
 
-    .cart-main {
-      .cart-th {
-        background: #f5f5f5;
-        border: 1px solid #ddd;
-        padding: 10px;
-        overflow: hidden;
-
-        &>div {
-          float: left;
-        }
-
-        .cart-th1 {
-          width: 25%;
-
-          input {
-            vertical-align: middle;
-          }
-
-          span {
-            vertical-align: middle;
-          }
-        }
-
-        .cart-th2 {
-          width: 25%;
-        }
-
-        .cart-th3,
-        .cart-th4,
-        .cart-th5,
-        .cart-th6 {
-          width: 12.5%;
-
-        }
-      }
-
-      .cart-body {
-        margin: 15px 0;
-        border: 1px solid #ddd;
-
-        .cart-list {
-          padding: 10px;
-          border-bottom: 1px solid #ddd;
-          overflow: hidden;
-
-          &>li {
-            float: left;
-          }
-
-          .cart-list-con1 {
-            width: 15%;
-          }
-
-          .cart-list-con2 {
-            width: 35%;
-
-            img {
-              width: 82px;
-              height: 82px;
-              float: left;
-            }
-
-            .item-msg {
-              float: left;
-              width: 150px;
-              margin: 0 10px;
-              line-height: 18px;
-            }
-          }
-
-
-          .cart-list-con4 {
-            width: 10%;
-
-          }
-
-          .cart-list-con5 {
-            width: 17%;
-
-            .mins {
-              border: 1px solid #ddd;
-              border-right: 0;
-              float: left;
-              color: #666;
-              width: 6px;
-              text-align: center;
-              padding: 8px;
-            }
-
-            input {
-              border: 1px solid #ddd;
-              width: 40px;
-              height: 33px;
-              float: left;
-              text-align: center;
-              font-size: 14px;
-            }
-
-            .plus {
-              border: 1px solid #ddd;
-              border-left: 0;
-              float: left;
-              color: #666;
-              width: 6px;
-              text-align: center;
-              padding: 8px;
-            }
-          }
-
-          .cart-list-con6 {
-            width: 10%;
-
-            .sum {
-              font-size: 16px;
-            }
-          }
-
-          .cart-list-con7 {
-            width: 13%;
-
-            a {
-              color: #666;
-            }
-          }
-        }
-      }
-    }
-
-    .cart-tool {
-      overflow: hidden;
+  .cart-main {
+    .cart-th {
+      background: #f5f5f5;
       border: 1px solid #ddd;
+      padding: 10px;
+      overflow: hidden;
 
-      .select-all {
-        padding: 10px;
-        overflow: hidden;
+      & > div {
         float: left;
+      }
 
-        span {
-          vertical-align: middle;
-        }
+      .cart-th1 {
+        width: 25%;
 
         input {
           vertical-align: middle;
         }
+
+        span {
+          vertical-align: middle;
+        }
       }
 
-      .option {
+      .cart-th2 {
+        width: 25%;
+      }
+
+      .cart-th3,
+      .cart-th4,
+      .cart-th5,
+      .cart-th6 {
+        width: 12.5%;
+      }
+    }
+
+    .cart-body {
+      margin: 15px 0;
+      border: 1px solid #ddd;
+
+      .cart-list {
         padding: 10px;
+        border-bottom: 1px solid #ddd;
         overflow: hidden;
-        float: left;
 
-        a {
+        & > li {
           float: left;
-          padding: 0 10px;
-          color: #666;
-        }
-      }
-
-      .money-box {
-        float: right;
-
-        .chosed {
-          line-height: 26px;
-          float: left;
-          padding: 0 10px;
         }
 
-        .sumprice {
-          width: 200px;
-          line-height: 22px;
-          float: left;
-          padding: 0 10px;
+        .cart-list-con1 {
+          width: 15%;
+        }
 
-          .summoney {
-            color: #c81623;
+        .cart-list-con2 {
+          width: 35%;
+
+          img {
+            width: 82px;
+            height: 82px;
+            float: left;
+          }
+
+          .item-msg {
+            float: left;
+            width: 150px;
+            margin: 0 10px;
+            line-height: 18px;
+          }
+        }
+
+        .cart-list-con4 {
+          width: 10%;
+        }
+
+        .cart-list-con5 {
+          width: 17%;
+
+          .mins {
+            border: 1px solid #ddd;
+            border-right: 0;
+            float: left;
+            color: #666;
+            width: 6px;
+            text-align: center;
+            padding: 8px;
+          }
+
+          input {
+            border: 1px solid #ddd;
+            width: 40px;
+            height: 33px;
+            float: left;
+            text-align: center;
+            font-size: 14px;
+          }
+
+          .plus {
+            border: 1px solid #ddd;
+            border-left: 0;
+            float: left;
+            color: #666;
+            width: 6px;
+            text-align: center;
+            padding: 8px;
+          }
+        }
+
+        .cart-list-con6 {
+          width: 10%;
+
+          .sum {
             font-size: 16px;
           }
         }
 
-        .sumbtn {
-          float: right;
+        .cart-list-con7 {
+          width: 13%;
 
           a {
-            display: block;
-            position: relative;
-            width: 96px;
-            height: 52px;
-            line-height: 52px;
-            color: #fff;
-            text-align: center;
-            font-size: 18px;
-            font-family: "Microsoft YaHei";
-            background: #e1251b;
-            overflow: hidden;
+            color: #666;
           }
         }
       }
     }
   }
+
+  .cart-tool {
+    overflow: hidden;
+    border: 1px solid #ddd;
+
+    .select-all {
+      padding: 10px;
+      overflow: hidden;
+      float: left;
+
+      span {
+        vertical-align: middle;
+      }
+
+      input {
+        vertical-align: middle;
+      }
+    }
+
+    .option {
+      padding: 10px;
+      overflow: hidden;
+      float: left;
+
+      a {
+        float: left;
+        padding: 0 10px;
+        color: #666;
+      }
+    }
+
+    .money-box {
+      float: right;
+
+      .chosed {
+        line-height: 26px;
+        float: left;
+        padding: 0 10px;
+      }
+
+      .sumprice {
+        width: 200px;
+        line-height: 22px;
+        float: left;
+        padding: 0 10px;
+
+        .summoney {
+          color: #c81623;
+          font-size: 16px;
+        }
+      }
+
+      .sumbtn {
+        float: right;
+
+        a {
+          display: block;
+          position: relative;
+          width: 96px;
+          height: 52px;
+          line-height: 52px;
+          color: #fff;
+          text-align: center;
+          font-size: 18px;
+          font-family: "Microsoft YaHei";
+          background: #e1251b;
+          overflow: hidden;
+        }
+      }
+    }
+  }
+}
 </style>
