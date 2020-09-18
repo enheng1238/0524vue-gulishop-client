b45f522cbcb69c135e5d6d1d8bb6eadfcf30cd2a
enheng1238 <223547544@qq.com>
2020-09-18 20:52:46 +0800

    element-ui 按需引入

--- src/main.js
+++ src/main.js
@@ -12,10 +12,21 @@ import store from '@/store'
 import '@/mock/mockServe' //运行mockServe文件 让mock可以注册为接口
 
 
-// 完整引入
+
+/*
+// element-ui 完整引入
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI)  //use 代表在声明使用插件
+*/
+
+// element-ui 按需引入
+// 1.下载一个babel的插件
+// 2.在babel.config.js当中配置插件
+// 3.引入需要使用的插件并注册
+
+import { Button } from 'element-ui';
+Vue.use(Button)
 
 reqCategoryList()
 
